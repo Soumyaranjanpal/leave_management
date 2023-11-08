6d135781a8d33efc37e226bd8328f0a5f5fc9928
Soumyaranjan Pal <spal@wolfram.com>
2023-11-08 18:51:31 +0530

    add initial error handler

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 require('dotenv').config();
-const express = require('express');
+
+const express = require("express");
+const routes = require("./routes/routes.js");
+require("./db/dbConn.js");
+const cors = require("cors");
+const errorMiddleware = require("./middleware/error.js");
 
 const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use("/", routes);
+
+// Middleware for error
+app.use(errorMiddleware);
+
 const port = process.env.PORT || 3000;
-app.get("/", (req, res) => {
-  res.send("Hello World");
-})
 
 app.listen(port, () => {
   console.log(`BE is listening on port ${port}`)
