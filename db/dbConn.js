064cb42a5d9c522a42bfe4e51c961fd81634199c
Soumyaranjan Pal <spal@wolfram.com>
2024-05-19 14:13:29 +0530

    save changes

--- db/dbConn.js
+++ db/dbConn.js
@@ -1,13 +1,12 @@
+const mongoose = require("mongoose");
 
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb+srv://root:root@cluster0.t4tlqrx.mongodb.net/?retryWrites=true&w=majority');
+mongoose.connect("mongodb://localhost:27017");
 
 const db = mongoose.connection;
 
-db.on('error', console.error.bind(console, 'Connection error:'));
-db.once('open', () => {
-  console.log('Connected to MongoDB');
+db.on("error", console.error.bind(console, "Connection error:"));
+db.once("open", () => {
+  console.log("Connected to MongoDB");
 });
 
 module.exports = db;
